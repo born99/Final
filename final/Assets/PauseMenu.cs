0cb49f8b49473275411348345533c864be1b2725
Nguyễn Minh Quân <56881880+zxquan123@users.noreply.github.com>
2021-09-17 14:50:22 +0700

    ...

---
+++ final/Assets/PauseMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIspause = false;
+    // Start is called before the first frame update
+    public GameObject pauseMenuUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIspause)
+            {
+                Resume();
+                
+            }
+            else
+            {
+                Pause();
+                
+            }
+            
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIspause = false;
+        Cursor.visible = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIspause = true;
+        Cursor.visible = true;
+    }
+    public void loadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("menu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
